68aded61b90a25c285dabaccb13e1063f9117e74
Daniel Lim <ddanielnp@hotmail.com>
2017-06-05 16:13:44 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -32,17 +32,28 @@ var checkForMatch = function () {
 	else {
 		alert("Sorry try again");
 	}
-}
+};
 
 var flipCard = function (cardId) {
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
 	cardsInPlay.push(cards[cardId].rank);
-  if (cardsInPlay.length === 2) {
+	this.setAttribute('src', cards[cardId].cardImage);
+	if (cardsInPlay.length === 2) {
   	checkForMatch();
   }
-}
+};
 
-flipCard(0);
-flipCard(2);
+var createBoard = function () {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		document.getElementById('game-board').appendChild(cardElement);
+		cardElement.addEventListener('click', flipCard);
+		}
+};
+
+createBoard();
